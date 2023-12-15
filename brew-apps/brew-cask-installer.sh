b71b98f3c218a2690b8db5de586a1c93b7e797ac
Zhengfa Dang <zfdang@users.noreply.github.com>
2023-12-15 19:17:26 +0800

    Update brew-cask-installer.sh

--- brew-apps/brew-cask-installer.sh
+++ brew-apps/brew-cask-installer.sh
@@ -6,35 +6,30 @@ brew install ffmpeg
 brew install git
 brew install handbrake
 brew install lux
-# brew install node
-# brew install openconnect
-# brew install python@3.11
+brew install smartmontools
 brew install wget
 
 ########### Brew Cask Install List ###########
 brew install --cask appcleaner
 brew install --cask baidunetdisk
-# brew install --cask bluesnooze
+brew install --cask countdown
 brew install --cask foxitreader
-brew install --cask foxmail
-brew install --cask free-download-manager
+brew install --cask thunder
 brew install --cask gimp
 brew install --cask hammerspoon
 brew install --cask iina
 brew install --cask iterm2
 brew install --cask karabiner-elements
 brew install --cask koodo-reader
-# brew install --cask macdown
-brew install --cask typora
+brew install --cask maccy
+brew install --cask maczip
 brew install --cask neteasemusic
 brew install --cask omnidisksweeper
 brew install --cask onyx
-# brew install --cask openconnect-gui
 brew install --cask raycast
-brew install --cask sublime-text
-# brew install --cask vlc
-brew install --cask xnviewmp
 brew install --cask snipaste
-brew install --cask maccy
+brew install --cask sublime-text
+brew install --cask typora
+brew install --cask visual-studio-code
 
 echo "And done..."
