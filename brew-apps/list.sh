34ecd18d9d8b5b126d674887b34a965ddcf57a2f
Zhengfa Dang <zfdang@users.noreply.github.com>
2024-08-09 09:32:08 +0800

    Update list.sh

--- brew-apps/list.sh
+++ brew-apps/list.sh
@@ -2,38 +2,39 @@
 
 ########### Brew Install List ###########
 brew install blueutil
-brew install exiftool
 brew install ffmpeg
 brew install git
 brew install handbrake
 brew install lux
 brew install node
 brew install openconnect
-brew install python@3.11
+brew install openjdk
+brew install openjdk@17
+brew install openvpn
+brew install smartmontools
 brew install wget
+brew install yt-dlp
 
 ########### Brew Cask Install List ###########
+brew install --cask android-studio
 brew install --cask appcleaner
 brew install --cask baidunetdisk
 brew install --cask balenaetcher
 brew install --cask foxitreader
-brew install --cask foxmail
-brew install --cask free-download-manager
 brew install --cask gimp
 brew install --cask hammerspoon
 brew install --cask iina
 brew install --cask iterm2
 brew install --cask karabiner-elements
 brew install --cask koodo-reader
+brew install --cask lulu
+brew install --cask maccy
 brew install --cask macdown
 brew install --cask neteasemusic
 brew install --cask omnidisksweeper
 brew install --cask onyx
-brew install --cask openconnect-gui
-brew install --cask raycast
-brew install --cask sogouinput
 brew install --cask sublime-text
 brew install --cask visual-studio-code
-brew install --cask vlc
-brew install --cask xnviewmp
+brew install --cask welly
+
 echo "And done..."
