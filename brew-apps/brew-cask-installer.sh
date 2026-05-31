c2bf717db6831999c1d06b1a75a0a1d3ed3e099e
dantifer <dantifer@gmail.com>
2026-05-31 15:04:51 +0800

    update list

--- brew-apps/brew-cask-installer.sh
+++ brew-apps/brew-cask-installer.sh
@@ -1,34 +1,46 @@
 #!/bin/bash
 
 ########### Brew Install List ###########
-brew install blueutil
+brew install btop
+brew install colima
+brew install colordiff
+brew install docker
+brew install docker-buildx
+brew install docker-compose
+brew install exiftool
 brew install ffmpeg
+brew install gh
 brew install git
+brew install git-filter-repo
+brew install git-lfs
+brew install handbrake
 brew install lux
-# brew install node
+brew install mtr
+brew install nexttrace
+brew install nmap
 brew install smartmontools
+brew install tree
 brew install wget
+brew install ykman
+
 
 ########### Brew Cask Install List ###########
-brew install --cask appcleaner
 brew install --cask baidunetdisk
+brew install --cask balenaetcher
 brew install --cask countdown
 brew install --cask foxitreader
-brew install --cask thunder
 brew install --cask gimp
 brew install --cask hammerspoon
 brew install --cask iina
 brew install --cask iterm2
 brew install --cask karabiner-elements
-brew install --cask koodo-reader
 brew install --cask maccy
-brew install --cask macdown
-brew install --cask maczip
 brew install --cask neteasemusic
-brew install --cask omnidisksweeper
-brew install --cask onyx
+brew install --cask qlmarkdown
 brew install --cask sublime-text
-brew install --cask shottr
 brew install --cask visual-studio-code
+brew install --cask thunder
+brew install --cask maczip
+brew install --cask shottr
 
 echo "Done..."
--- brew-apps/list.sh
+++ brew-apps/list.sh
@@ -1,25 +1,66 @@
 #!/bin/bash
-
 ########### Brew Install List ###########
+brew install argocd
 brew install blueutil
+brew install btop
+brew install caddy
+brew install colima
+brew install colordiff
+brew install docker
+brew install docker-buildx
+brew install docker-compose
+brew install duti
+brew install exiftool
 brew install ffmpeg
+brew install gemini-cli
+brew install gh
 brew install git
+brew install git-filter-repo
+brew install git-lfs
+brew install go
+brew install graphviz
 brew install handbrake
+brew install helm
+brew install icdiff
+brew install imagemagick
+brew install iperf3
+brew install kubernetes-cli
 brew install lux
-brew install node
+brew install mkvtoolnix
+brew install mtr
+brew install mysql-client
+brew install nexttrace
+brew install nmap
 brew install openconnect
 brew install openjdk
 brew install openjdk@17
 brew install openvpn
+brew install php
+brew install pinentry-mac
+brew install poppler
+brew install python@3.10
+brew install railway
+brew install rbenv
+brew install ripgrep
 brew install smartmontools
+brew install sox
+brew install swag
+brew install tree
 brew install wget
+brew install ykman
+brew install youtube-dl
 brew install yt-dlp
-
+brew install zfdang/free-for-macos/free-for-macos
+brew install zoxide
 ########### Brew Cask Install List ###########
 brew install --cask android-studio
 brew install --cask appcleaner
 brew install --cask baidunetdisk
 brew install --cask balenaetcher
+brew install --cask cc-switch
+brew install --cask countdown
+brew install --cask easydict
+brew install --cask font-fira-code
 brew install --cask foxitreader
 brew install --cask gimp
 brew install --cask hammerspoon
@@ -27,14 +68,18 @@ brew install --cask iina
 brew install --cask iterm2
 brew install --cask karabiner-elements
 brew install --cask koodo-reader
-brew install --cask lulu
 brew install --cask maccy
 brew install --cask macdown
+brew install --cask maczip
+brew install --cask meetingbar
+brew install --cask miaoyan
 brew install --cask neteasemusic
+brew install --cask netnewswire
 brew install --cask omnidisksweeper
-brew install --cask onyx
+brew install --cask qlmarkdown
+brew install --cask quickrecorder
 brew install --cask sublime-text
 brew install --cask visual-studio-code
+brew install --cask wechattweak-cli
 brew install --cask welly
-
 echo "And done..."
